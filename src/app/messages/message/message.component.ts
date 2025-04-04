c5c22c627ce343d834f19a8e64bc059606c5c66d
Istvan078 <127589069+Istvan078@users.noreply.github.com>
2025-04-04 18:05:19 +0200

    Styling

--- src/app/messages/message/message.component.ts
+++ src/app/messages/message/message.component.ts
@@ -7,74 +7,76 @@ import { UserClass } from 'src/app/models/user.model';
 import { BaseService } from 'src/app/services/base.service';
 
 @Component({
- selector: 'app-message',
- templateUrl: './message.component.html',
- styleUrls: ['./message.component.scss'],
- standalone: false,
+  selector: 'app-message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.scss'],
+  standalone: false,
 })
 export class MessageComponent implements OnInit {
- @Input() matches: UserClass[] = []
- @Input() matchProfile?: UserClass;
- @Input() options?: Options 
- messages: Messages = new Messages([]);
- message: Message = new Message();
- uProf?: UserClass;
- constructor(private base: BaseService) {}
+  @Input() matches: UserClass[] = []
+  @Input() matchProfile?: UserClass;
+  @Input() options?: Options
+  messages: Messages = new Messages([]);
+  message: Message = new Message();
+  uProf?: UserClass;
+  constructor(private base: BaseService) { }
 
- ngOnInit() {
-  this.base.userProfBehSubj.subscribe((uProf) => {
-   if (uProf) {
-    this.uProf = uProf
-    this.getMessages();
-    this.watchMatchProfChange()
-    console.log(this.messages);
-  };
-  });
- }
+  ngOnInit() {
+    this.base.userProfBehSubj.subscribe((uProf) => {
+      if (uProf) {
+        this.uProf = uProf
+        this.getMessages();
+        this.watchMatchProfChange()
+        console.log(this.messages);
+      };
+    });
+  }
 
- async getMessages() {
- this.messages.messages = await this.base.getMessages(this.uProf?.uid!, this.uProf?.email!, this.matchProfile?.uid!, this.matchProfile?.email!)
- }
+  async getMessages() {
+    this.messages.messages = await this.base.getMessages(this.uProf?.uid!, this.uProf?.email!, this.matchProfile?.uid!, this.matchProfile?.email!)
+  }
 
- watchMatchProfChange() {
-  let oldMatchUid = this.matchProfile?.uid
-  const int = setInterval(() => {
-    if(this.matchProfile?.uid !== oldMatchUid) {
-      this.getMessages();
-      oldMatchUid = this.matchProfile?.uid;
-      console.log(`***MASIK FELHASZNALONAK IROK****`);
-    }
-  }, 200);
- }
+  watchMatchProfChange() {
+    let oldMatchUid = this.matchProfile?.uid
+    const int = setInterval(() => {
+      if (this.matchProfile?.uid !== oldMatchUid) {
+        this.getMessages();
+        oldMatchUid = this.matchProfile?.uid;
+        console.log(`***MASIK FELHASZNALONAK IROK****`);
+      }
+    }, 200);
+  }
 
- addMessagesWithMatch() {
-  this.base.addMessagesWithMatch(
-   this.uProf?.uid!,
-   this.uProf?.email!,
-   this.matchProfile?.uid!,
-   this.messages.setMessagesForFirestore()
-  );
- }
- selectMatch(match: UserClass) {
-  this.matchProfile = match;
-  this.options!.isSelectedMatch = true;
- }
- backToMsgs() {
-  this.options!.isSelectedMatch = false
- }
+  addMessagesWithMatch() {
+    this.base.addMessagesWithMatch(
+      this.uProf?.uid!,
+      this.uProf?.email!,
+      this.matchProfile?.uid!,
+      this.messages.setMessagesForFirestore()
+    );
+  }
+  selectMatch(match: UserClass) {
+    this.matchProfile = match;
+    this.options!.isSelectedMatch = true;
+  }
+  backToMsgs() {
+    this.options!.isSelectedMatch = false
+  }
 
- onMessageSend(form: NgForm) {
+  onMessageSend(form: NgForm) {
     this.message = {
-        message: form.value.message,
-        senderUid: this.uProf?.uid!,
-        sentToUid: this.matchProfile?.uid!,
-        number: this.messages.messages[this.messages.messages.length - 1].number + 1
+      message: form.value.message,
+      senderUid: this.uProf?.uid!,
+      sentToUid: this.matchProfile?.uid!,
+      number: this.messages.messages.length ? this.messages.messages[this.messages.messages.length - 1].number + 1 : 1,
+      sentAt: new Date(),
+      isSent: true,
     }
-  this.messages.messages.push(this.message)
-  this.addMessagesWithMatch();
-  const messNumber = this.message.number;
-  this.message = new Message();
-  this.message.number = messNumber + 1;
-  form.resetForm()
- }
+    this.messages.messages.push(this.message)
+    this.addMessagesWithMatch();
+    const messNumber = this.message.number;
+    this.message = new Message();
+    this.message.number = messNumber + 1;
+    form.resetForm()
+  }
 }
--- src/app/models/message.model.ts
+++ src/app/models/message.model.ts
@@ -1,14 +1,25 @@
 export class Message {
- senderUid: string = "";
- sentToUid: string = "";
- message: string = "";
- number: number = 0;
+    senderUid: string = "";
+    sentToUid: string = "";
+    message: string = "";
+    number: number = 0;
+    sentAt: Date = new Date();
+    attachments?: string[];
+    isRead?: boolean;
+    isSent?: boolean;
+    isReceived?: boolean;
+    isDeleted?: boolean;
+    isStarred?: boolean;
+    isEdited?: boolean;
+    isTyping?: boolean;
+    readAt?: Date;
+
 }
 export class Messages {
- constructor(public messages: Message[]) {}
- setMessagesForFirestore() {
-  const copy = { ...this.messages };
-  console.log(copy)
-  return copy;
- }
+    constructor(public messages: Message[]) { }
+    setMessagesForFirestore() {
+        const copy = { ...this.messages };
+        console.log(copy)
+        return copy;
+    }
 }
