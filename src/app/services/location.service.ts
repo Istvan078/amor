10368806eb6c3d4bfbf43166deefc8076d416204
Istvan078 <kalmaristvan078@gmail.com>
2026-05-25 22:23:34 +0100

    Fixed location permission bug on android

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -3,89 +3,97 @@ import { Injectable } from '@angular/core';
 import { Geolocation, Position } from '@capacitor/geolocation';
 
 @Injectable({
- providedIn: 'root',
+  providedIn: 'root',
 })
 export class LocationService {
- constructor(private http: HttpClient) {
-  // this.getCoordsGeocodeXYZ();
- }
+  constructor(private http: HttpClient) {
+    // this.getCoordsGeocodeXYZ();
+  }
 
- async getLocation(): Promise<Position> {
-  const position = await Geolocation.getCurrentPosition();
-  console.log(position);
-  return position;
- }
-
- getDistanceBetweenPoints = (
-  lat1: number,
-  lon1: number,
-  lat2: number,
-  lon2: number
- ) => {
-  const R = 6371; // A Föld sugara km-ben
-  const dLat = (lat2 - lat1) * (Math.PI / 180);
-  const dLon = (lon2 - lon1) * (Math.PI / 180);
-
-  const a =
-   Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-   Math.cos(lat1 * (Math.PI / 180)) *
-    Math.cos(lat2 * (Math.PI / 180)) *
-    Math.sin(dLon / 2) *
-    Math.sin(dLon / 2);
-
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-  return R * c; // Eredmény km-ben
- };
-
- getLocName(position: Position, isOpenSMap?: boolean) {
-  return new Promise((res, rej) => {
-   const url = !isOpenSMap
-    ? `https://geocode.xyz/${position.coords.latitude},${position.coords.longitude}?json=1&auth=469206953456508521936x26064`
-    : `https://nominatim.openstreetmap.org/reverse?format=json&lat=${position.coords.latitude}&lon=${position.coords.longitude}`;
-
-   this.http.get(url).subscribe((location) => {
-    res(location);
-   });
-  });
- }
-
- getCoordsGeocodeXYZ = async (address: string) => {
-  return new Promise<any>((res, rej) => {
-   const url = `https://geocode.xyz/${address}?json=1&auth=469206953456508521936x26064`;
-   try {
-    this.http.get(url).subscribe((data: any) => {
-     if (data) {
-      if (data.latt == '0.00000') res(new Error('Lejart a napi limit'));
-      const location = { lat: data.latt, lon: data.longt };
-      res(location);
-     } else {
-      res('Hiba');
-     }
+  async getLocation(): Promise<Position> {
+    let permission = await Geolocation.checkPermissions();
+    if (permission.location !== "granted")
+      permission = await Geolocation.requestPermissions({ permissions: ['location'] });
+    if (permission.location !== "granted")
+      throw new Error('Location permission was denied');
+    const position = await Geolocation.getCurrentPosition({
+      enableHighAccuracy: false,
+      timeout: 15000,
+      maximumAge: 60000,
     });
-   } catch (error) {
-    res(error);
-   }
-  });
- };
+    return position;
+  }
 
- delay = (millisec: number) => new Promise((res) => setTimeout(res, millisec));
+  getDistanceBetweenPoints = (
+    lat1: number,
+    lon1: number,
+    lat2: number,
+    lon2: number
+  ) => {
+    const R = 6371; // A Föld sugara km-ben
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLon = (lon2 - lon1) * (Math.PI / 180);
 
- getCoordinatesOSM = async (address: string) => {
-  return new Promise<any>((res, rej) => {
-   const url = `https://nominatim.openstreetmap.org/search?format=json&q=${address}`;
-   try {
-    this.http.get(url).subscribe((data: any) => {
-     if (data.length > 0) {
-      const location = data[0];
-      res(location);
-     } else {
-      res('Hiba');
-     }
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(lat1 * (Math.PI / 180)) *
+      Math.cos(lat2 * (Math.PI / 180)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return R * c; // Eredmény km-ben
+  };
+
+  getLocName(position: Position, isOpenSMap?: boolean) {
+    return new Promise((res, rej) => {
+      const url = !isOpenSMap
+        ? `https://geocode.xyz/${position.coords.latitude},${position.coords.longitude}?json=1&auth=469206953456508521936x26064`
+        : `https://nominatim.openstreetmap.org/reverse?format=json&lat=${position.coords.latitude}&lon=${position.coords.longitude}`;
+
+      this.http.get(url).subscribe((location) => {
+        res(location);
+      });
     });
-   } catch (error) {
-    res(error);
-   }
-  });
- };
+  }
+
+  getCoordsGeocodeXYZ = async (address: string) => {
+    return new Promise<any>((res, rej) => {
+      const url = `https://geocode.xyz/${address}?json=1&auth=469206953456508521936x26064`;
+      try {
+        this.http.get(url).subscribe((data: any) => {
+          if (data) {
+            if (data.latt == '0.00000') res(new Error('Lejart a napi limit'));
+            const location = { lat: data.latt, lon: data.longt };
+            res(location);
+          } else {
+            res('Hiba');
+          }
+        });
+      } catch (error) {
+        res(error);
+      }
+    });
+  };
+
+  delay = (millisec: number) => new Promise((res) => setTimeout(res, millisec));
+
+  getCoordinatesOSM = async (address: string) => {
+    return new Promise<any>((res, rej) => {
+      const url = `https://nominatim.openstreetmap.org/search?format=json&q=${address}`;
+      try {
+        this.http.get(url).subscribe((data: any) => {
+          if (data.length > 0) {
+            const location = data[0];
+            res(location);
+          } else {
+            res('Hiba');
+          }
+        });
+      } catch (error) {
+        res(error);
+      }
+    });
+  };
 }
